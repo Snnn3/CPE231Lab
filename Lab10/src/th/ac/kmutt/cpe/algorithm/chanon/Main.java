c0506cd316e31029772780a29408b9cd3a3a65e1
Chanon Lhumsa-ard <l.chanon2549@gmail.com>
2025-11-15 22:12:51 +0700

    lab10

--- Lab10/src/th/ac/kmutt/cpe/algorithm/chanon/Main.java
+++ Lab10/src/th/ac/kmutt/cpe/algorithm/chanon/Main.java
@@ -1,6 +1,8 @@
 package th.ac.kmutt.cpe.algorithm.chanon;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,22 +10,57 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] com = new int[n][n];
-        int[][] stu = new int[n][n];        
+        int[][] stu = new int[n][n];       
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 stu[i][j] = sc.nextInt();
             }
         }
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 com[i][j] = sc.nextInt();
             }
         }
-
-
-
         
+        ArrayList<Integer> freeS = new ArrayList<>();
+        int[] selected = new int[n];
+        Arrays.fill(selected, -1);
+        for (int i = 0; i < n; i++) {
+            freeS.add(i); 
+        }
 
+        while (!freeS.isEmpty()) {
+            int s = freeS.removeFirst();
+            int c = -1;
+            for (int i = 0; i < n; i++) {
+                if (stu[s][i] != 99) {
+                    c = stu[s][i];
+                    stu[s][i] = 99;
+                    break;
+                }
+            }
+            if (selected[c] == -1) {
+                selected[c] = s;
+            }else{
+                int oldS = selected[c];
+               for (int i = 0; i < n; i++) {
+                    if (com[c][i] == s) {
+                        selected[c] = s;
+                        freeS.add(oldS);
+                        break;
+                    } else if (com[c][i] == oldS) {
+                        freeS.add(s);
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        for (int i = 0; i < selected.length; i++) {
+            System.out.println(i + " " + selected[i]);
+        }
 
         sc.close();
     }
